733b86b8f03dad19af829bdba235c0675a042eda
zxlai <garylai1990@gmail.com>
2014-11-28 21:16:27 +0800

    Complete function.

--- lib/jiazi.rb
+++ lib/jiazi.rb
@@ -1,5 +1,26 @@
 require "jiazi/version"
 
+require 'open-uri'
+require 'json'
+
 module Jiazi
-  # Your code goes here...
+  
+  def self.title
+    self.fetch(1)
+  end
+
+  def self.paragraph
+    self.fetch(1, 400, 500)
+  end
+
+  def self.custom(number, min, max)
+    self.fetch(number, min, max)
+  end
+
+  private
+
+  def self.fetch(number = 1, min = 10, max = 20)
+    JSON.load(open("http://more.handlino.com/sentences.json?n=#{number}&limit=#{min},#{max}"))
+  end
+
 end
