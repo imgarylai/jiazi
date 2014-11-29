71d7c78908143c6b1335dae5e7bb6fef8ccd77ca
zxlai <garylai1990@gmail.com>
2014-11-29 10:37:17 +0800

    spec

--- spec/jiazi_spec.rb
+++ spec/jiazi_spec.rb
@@ -1,10 +1,20 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Jiazi do
   
-  it '#custom' do
+  it "#custom" do
     expect(Jiazi.custom(1, 2, 1))
       .to eq "max should be greater or equal to min."
+  end
+
+  it "#title" do 
+    expect(Jiazi.title.length)
+      .to eq (Jiazi.custom(1).length)
+  end
+
+  it "#paragraph" do 
+    expect(Jiazi.paragraph.length)
+      .to eq (Jiazi.custom(1, 400, 500).length)
   end
 
 end
